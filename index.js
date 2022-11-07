806ad37b24e5b9fe1a82c8901d7544cbbb453930
salmatonka <108433658+salmatonka@users.noreply.github.com>
2022-11-08 03:34:14 +0600

    second commit

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express =require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -7,6 +8,29 @@ const port = process.env.PORT || 5000;
 //middleware
 app.use(cors());
 app.use(express.json());
+
+
+
+const uri = `mongodb+srv://weddingPhotographer:u9ppmpVvmcyLIDJ8@cluster0.ankd1bs.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri)
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+         const photoCollection = client.db('weddingPhotographer').collection('services')
+         app.get('/photos',async(req,res)=>{
+            const query = {}
+            const cursor = photoCollection.find(query)
+            const photos = await cursor.toArray()
+            res.send(photos)
+         })
+    }
+    finally{
+
+    }
+}
+ run ().catch(err=>console.error(err))
+
 
 app.get('/',(req,res)=>{
     res.send('hello photographer');
