99f064b81d5efc3ff0a788c5db5fb45218fd0ec1
salmatonka <108433658+salmatonka@users.noreply.github.com>
2022-11-10 06:45:17 +0600

    third commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express =require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -18,12 +18,32 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run(){
     try{
          const photoCollection = client.db('weddingPhotographer').collection('services')
-         app.get('/photos',async(req,res)=>{
+         app.get('/',async(req,res)=>{
             const query = {}
             const cursor = photoCollection.find(query)
-            const photos = await cursor.toArray()
-            res.send(photos)
+            const services = await cursor.limit(3).toArray()
+            res.send(services)
          })
+
+         app.get('/services',async(req,res)=>{
+            const query = {}
+            const cursor = photoCollection.find(query)
+            const services = await cursor.toArray()
+            res.send(services)
+         })
+
+
+         app.get('/services/:id',async(req,res)=>{
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)}
+            const allDetails = await photoCollection.findOne(query)
+            res.send(allDetails)
+         })
+
+
+
+
+
     }
     finally{
 
